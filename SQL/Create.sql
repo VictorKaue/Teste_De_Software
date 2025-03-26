fc2ffdc2b50962b582fa818a975e3131a35340df
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2025-03-26 10:29:01 -0300

    Update Create.sql

--- SQL/Create.sql
+++ SQL/Create.sql
@@ -7,3 +7,12 @@ CREATE TABLE medico(
     crm VARCHAR(20) NOT NULL,
 	data_nascimento DATE
 );
+
+CREATE TABLE paciente(
+	cpf INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    nome VARCHAR(100) NOT NULL,
+    dataNascimento DATE NOT NULL,
+    id_medico INT NOT NULL,
+    FOREIGN KEY (id_medico) REFERENCES medico(id_medico)
+);
+
