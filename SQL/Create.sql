e264a61a616eaeb141fe75a2ce820ee93cae7e73
VictorKaue <98410524+VictorKaue@users.noreply.github.com>
2025-03-26 10:30:41 -0300

    Update Create.sql

--- SQL/Create.sql
+++ SQL/Create.sql
@@ -2,17 +2,16 @@ CREATE DATABASE sismed;
 USE sismed;
 
 CREATE TABLE medico(
-	id_medico INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+    crm VARCHAR(20) PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(30) NOT NULL,
-    crm VARCHAR(20) NOT NULL,
-	data_nascimento DATE
+    data_nascimento DATE
 );
 
 CREATE TABLE paciente(
 	cpf INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
     nome VARCHAR(100) NOT NULL,
     dataNascimento DATE NOT NULL,
-    id_medico INT NOT NULL,
-    FOREIGN KEY (id_medico) REFERENCES medico(id_medico)
+    id_medico VARCHAR(20) NOT NULL,
+    FOREIGN KEY (id_medico) REFERENCES medico(crm)
 );
 
